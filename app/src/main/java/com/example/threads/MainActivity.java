22117a63cd3d70de855fe545a79af05287cb2a54
pmunuera <95918506+pmunuera@users.noreply.github.com>
2022-10-14 19:28:52 +0200

    Commit Thread

--- app/src/main/java/com/example/threads/MainActivity.java
+++ app/src/main/java/com/example/threads/MainActivity.java
@@ -3,8 +3,14 @@ package com.example.threads;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -13,28 +19,54 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> listilla;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        listilla = new ArrayList<>();
+        listilla.add("hola");
+        listilla.add("Mu lista tu eres");
+
+        ArrayAdapter<String>  adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,listilla);
+        //ListView lv = findViewById(R.id.textView2);
+        //lv.setAdapter(adapter);
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        Handler handler = new Handler(Looper.getMainLooper());
+
         Button b = (Button) findViewById(R.id.button);
 
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                getDataFromUrl("https://api.myip.com");
+
+
+                executor.execute(new Runnable() {
+                    @Override
+                    public void run() {
+                        //Background work here
+                        String res = getDataFromUrl("https://api.myip.com");
+                        Log.i("MYAPP",res);
+                        TextView tv = (TextView) findViewById(R.id.textView);
+                        tv.append(res);
+                    }
+                });
             }
         });
+
     }
     String error = ""; // string field
     private String getDataFromUrl(String demoIdUrl) {
-
         String result = null;
         int resCode;
         InputStream in;
